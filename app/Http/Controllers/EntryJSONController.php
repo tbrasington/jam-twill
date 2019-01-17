87a7233143c3a65a8127cd1207db9649a87f44b6
Thomas Brasington <tom@2c2d.co.uk>
2019-01-17 19:06:52 +0000

    on index

--- app/Http/Controllers/EntryJSONController.php
+++ app/Http/Controllers/EntryJSONController.php
@@ -11,17 +11,41 @@ use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
 class EntryJSONController extends Controller
 {
     //
+    // public function old_index() {
+    //     $entry=Entry::where('published', true)
+    //     ->with(['medias'=>function($q){
+    //       $q->select(['medias.id', 'uuid','caption','alt_text', 'role','media_id', 'mediable_id']);
+    //     }])
+    //     ->with('content')
+    //     ->with(['slugs'=>function ($q) {
+    //         $q->where('active', 1);
+    //     }])->get();
+    
+    //     return $entry;
+    // }
+
     public function index() {
-        $entry=Entry::where('published', true)
+        $entries = Entry::where('published', true)
         ->with(['medias'=>function($q){
           $q->select(['medias.id', 'uuid','caption','alt_text', 'role','media_id', 'mediable_id']);
         }])
-        ->with('content')
         ->with(['slugs'=>function ($q) {
             $q->where('active', 1);
         }])->get();
+
+        foreach ($entries as $entry) {
+            
+            $blocks = $entry->content()->get();
+            foreach ($blocks as $block) {
+                $childBlocks = $block->where('parent_id', $block->id)->get();
+                $block->blocks = $childBlocks;
+            }
+
+            $entry->blocks =  $blocks; 
+            
+        }
     
-        return $entry;
+        return $entries;
     }
 
 
